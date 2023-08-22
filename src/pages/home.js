63a2565524e62c10181d4cee3719252b74162db7
Dikshya Sharma <dikxu22@gmail.com>
2023-08-22 14:54:17 +0545

    updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,81 +1,56 @@
+import React from 'react';
 import img from "../bodyimg.svg";
 import { Footer } from './footer';
 import { Navbarr } from '../Navbar';
 import { Edit } from "./edit";
-import { useNavigate} from "react-router";
+import { useNavigate } from "react-router-dom"; // Make sure you're importing from 'react-router-dom'
 
-export const Home = () => {
-  let navigate = useNavigate();
+export const Home = ({ handleChange, addTask, listTasks, deleteTask }) => {
+  const navigate = useNavigate();
+
   const routeChange = () => {
-    let path = "./edit";
+    let path = "/edit"; // Correct the path to use the absolute path or relative path as per your routing setup
     navigate(path);
-  }
-    return(
-      <div className="homePage">
-        <Navbarr />
-        <div className='body'>
-<div className='sectionI'>
-  <img src= {img} className='imageBody' alt='image' height="700px" width= "500px" />
-  </div>
-<div className='sectionII'>
-<p class="h1 text-center mt-3 mb-4 pb-3 text-primary">
-              <u className='title'>Task-List</u>
-            </p>
-            <form class="d-flex justify-content-center align-items-center mb-4" className='inputHome'>
-              <div class="form-outline flex-fill">
-                <input type="text" id="form3" class="form-control form-control-lg" placeholder='New Task...' />
-              </div>
-              <input className='addButton' type='button' value="ADD" />
-            </form>
-<br/>
-  <br/>
-           <div className='list'>
-          <ul className="listItems">
-            <li>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label for="vehicle1" className="labels">Do Assignment</label>
-            <span className="buttonsED">
-            <button type="button" class="btn btn-primary" onClick={routeChange} >Edit</button>
-            &nbsp; &nbsp;
-            <button type="button" class="btn btn-danger">Delete</button>
-            <br />
-            </span>
-            </li>
-            <li>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label for="vehicle1" className="labels">Do Laundry</label>
-            <span className="buttonsED">
-            <button type="button" class="btn btn-primary" onClick={routeChange}>Edit</button>
-            &nbsp; &nbsp;
-            <button type="button" class="btn btn-danger">Delete</button>
-            <br />
-            </span>
-            </li>
-            <li>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label for="vehicle1" className="labels">Cook Dinner</label>
-            <span className="buttonsED">
-            <button type="button"  class="btn btn-primary" onClick={routeChange}>Edit</button>
-            &nbsp; &nbsp;
-            <button type="button" class="btn btn-danger">Delete</button>
-            <br />
-            </span>
-            </li>
-            <li>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label for="vehicle1" className="labels">Read Books</label>
-            <span className="buttonsED">
-            <button type="button" class="btn btn-primary" onClick={routeChange}>Edit</button>
-            &nbsp; &nbsp;
-            <button type="button" class="btn btn-danger">Delete</button>
-            <br />
-            </span>
-            </li>
-          </ul>
-            </div>
-            </div>
-</div>
-<Footer />
-      </div>
-    )
   };
+
+  return (
+    <div className="homePage">
+      <Navbarr />
+      <div className='body'>
+        <div className='sectionI'>
+          <img src={img} className='imageBody' alt='image' height="700px" width="500px" />
+        </div>
+        <div className='sectionII'>
+          <p className="h1 text-center mt-3 mb-4 pb-3 text-primary">
+            <u className='title'>Task-List</u>
+          </p>
+          <form className="d-flex justify-content-center align-items-center mb-4 inputHome">
+            <div className="form-outline flex-fill">
+              <input onChange={handleChange} type="text" id="form3" className="form-control form-control-lg" placeholder='New Task...' />
+            </div>
+            <input onClick={addTask} className='addButton' type='button' value="ADD" />
+          </form>
+          <br />
+          <br />
+
+          <ul className="list">
+            {listTasks.map((task) => (
+              <li key={task.id} className="listItems">
+                <input className="form-check-input" type="checkbox" value="" id={`flexCheck${task.id}`} />
+                {task.taskName}
+                <span className="buttonsED">
+                  <button type="button" className="btn btn-primary" onClick={routeChange}>Edit</button>
+                  &nbsp; &nbsp;
+                  <button onClick={() => deleteTask(task.id)} type="button" className="btn btn-danger">Delete</button>
+                  <br />
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <Footer />
+
+    </div>
+  );
+};
